2d3601e2d025a9773d3065ca4404f3b3b3036034
joonsooyoo <sandiegojs@gmail.com>
2026-05-27 14:42:31 +0900

    Update Apps Script deployment URL

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,3 @@
 window.SE_EVAL_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyWUyFoAPft43PNvZ297tfErT7wmf083G1VmGp7XbkXpxafmFOGX2FtNaAnqh8G1Kv0_w/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxMKpVf3E4uKK7tVpwhIixmcj6C0YvgwPD5sr-YrAv4L2YM4qTpigy-fDh5FzTN71MPpA/exec",
 };
