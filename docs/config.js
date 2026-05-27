bddd94a7b2910c822f0f31ce08a9a5d95d3a4347
joonsooyoo <sandiegojs@gmail.com>
2026-05-27 14:26:47 +0900

    Connect Google Apps Script backend

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,3 @@
 window.SE_EVAL_CONFIG = {
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyWUyFoAPft43PNvZ297tfErT7wmf083G1VmGp7XbkXpxafmFOGX2FtNaAnqh8G1Kv0_w/exec",
 };
